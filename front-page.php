929d95ad4f8d62ba1937d9b4afec3b0e246b291a
Alvaro-FZ <151030461+Alvaro-FZ@users.noreply.github.com>
2025-06-04 22:49:19 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -2,9 +2,9 @@
 
 <!-- CONTENIDO PRINCIPAL -->
  <div class="theme-switch">
-    <input type="checkbox" id="theme-toggle">
+    <input type="checkbox" id="theme-toggle" name="theme-toggle">
     <label for="theme-toggle"></label>
-    <span>Cambiar tema</span>
+    <span for="theme-toggle">Cambiar tema</span>
 </div>
 <div class="main-container">
     <!-- PANEL IZQUIERDO -->
@@ -44,11 +44,12 @@
     </div>
 
     <!-- PANEL DERECHO -->
+     
+    <!-- AÑADIR IMAGEN DEL CORAZON PARA IMPLEMENTAR LA OPCION DE CARDIO DE LA API -->
+
     <div class="right-panel">
         <img id="muscle-image" src="<?php echo get_template_directory_uri(); ?>/IMG/MuscleGroupBaseImageTransparentBackground.png" class="imagen-cuerpo" alt="Silueta cuerpo humanoq" usemap="#cuerpo">
-        <!-- <img src="./IMG/MuscleGroupBaseImageTransparentBackground.png" alt="Silueta cuerpo humano" class="imagen-cuerpo" usemap="#cuerpo" /> -->
         <map name="cuerpo">
-            <area alt="Cabeza" title="Cabeza" onclick="mostrarEjercicios('cardio')" coords="431,169,528,169,541,267,535,321,491,324,428,314,415,245" shape="poly" />
             <area alt="Biceps" title="Biceps" onclick="mostrarEjercicios('upper%20arms')" coords="297,577,275,640,253,654,239,642,244,588,263,547,285,537,293,554" shape="poly">
             <area alt="Biceps" title="Biceps" onclick="mostrarEjercicios('upper%20arms')" coords="678,536,668,573,685,626,700,649,723,653,725,624,721,593,713,571,696,546" shape="poly">
             <area alt="Pecho" title="Pecho" onclick="mostrarEjercicios('chest')" coords="434,420,454,425,468,445,471,459,475,479,475,498,473,518,470,538,461,557,443,571,417,574,393,574,370,569,354,557,348,542,339,523,339,505,339,482,341,466,348,449,363,437,375,430,388,423,409,420" shape="poly">
